79ad5a0a742d6d63085df5eccdc1bb332a769d7e
xancxoxin <judy010321@naver.com>
2026-04-30 15:44:29 +0900

    refactor: HomeContentSection 레이아웃 단순화 및 ImmutableList 적용

--- app/src/main/java/com/example/letssopt/week2/home/component/content/HomeContentSection.kt
+++ app/src/main/java/com/example/letssopt/week2/home/component/content/HomeContentSection.kt
@@ -1,23 +1,31 @@
 package com.example.letssopt.week2.home.component.content
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.ImmutableList
 
 @Composable
-fun HomeContentSection(contents: List<Int>) {
-    Column {
-        LazyRow(
-            contentPadding = PaddingValues(horizontal = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            items(contents) { imageRes ->
-                HomeContentItem(imageRes = imageRes)
-            }
+fun HomeContentSection(
+    // ImmutableList로 변경
+    contents: ImmutableList<Int>,
+    modifier: Modifier = Modifier
+) {
+    // Column 제거 -> LazyRow 최상위 배치
+    LazyRow(
+        modifier = modifier,
+        contentPadding = PaddingValues(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(
+            items = contents,
+            key = { imageRes -> imageRes }
+        ) { imageRes ->
+            HomeContentItem(imageRes = imageRes)
         }
     }
 }
